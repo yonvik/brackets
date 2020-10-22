d0063691de060ef61813c4261e4e8ba2da0933c8
Andrey Yankovskiy <tiamoonrock@gmail.com>
2020-10-22 19:54:34 +0300

    init: completed test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const pairsRegExp = bracketsConfig
+      .reduce((acc, [open, close]) => {
+          const openBracket = ["(", "|", ")", "[", "]", "{", "}"].includes(
+              open
+          )
+              ? `\\${open}`
+              : open;
+          const closeBracket = ["(", "|", ")", "[", "]", "{", "}"].includes(
+              close
+          )
+              ? `\\${close}`
+              : close;
+          return acc.concat(`(${openBracket}${closeBracket})`);
+      }, [])
+      .join("|");
+
+  const regExp = new RegExp(pairsRegExp, "g");
+
+  function searchPair(str) {
+      if (str && regExp.test(str)) return searchPair(str.replace(regExp, ""));
+      return str.length === 0;
+  }
+  const isCheck = searchPair(str);
+
+  return isCheck;
 }
+
+module.exports = check;
